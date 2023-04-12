0fd1fe6d993d9703a15fe567e0cef5b3b51adf5b
Ulises Daniel García Méndez <84304055+UliGarci@users.noreply.github.com>
2023-04-12 10:51:55 -0600

    Update TablesCategory.js

--- src/shared/Components/TablesCategory.js
+++ src/shared/Components/TablesCategory.js
@@ -10,7 +10,7 @@ export const TablesCategory = () => {
 
     useEffect(() => {
       const fetchData = async ()=>{
-        const result = await axios('http://34.201.151.124:4000/shop/category');
+        const result = await axios('http://34.201.251.124:5000/shop/category');
         setData(result.data);
         console.log('obtenido=>',result.data);
       };
