a89ab7a65476d54394112a1dc5bc527ec3e82998
Ulises Daniel García Méndez <84304055+UliGarci@users.noreply.github.com>
2023-04-12 10:51:42 -0600

    Update TablesBuy.js

--- src/shared/Components/TablesBuy.js
+++ src/shared/Components/TablesBuy.js
@@ -10,7 +10,7 @@ export const TablesBuy = () => {
 
     useEffect(() => {
       const fetchData = async ()=>{
-        const result = await axios('http://34.201.151.124:4000/shop/buy');
+        const result = await axios('http://34.201.251.124:4000/shop/buy');
         setData(result.data);
         console.log('obtenido=>',result);
         console.log('obtenido 2=>',result.data);
