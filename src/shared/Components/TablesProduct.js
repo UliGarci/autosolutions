3f09f050c7a335a692f7e5ff95dfed4c464228e6
Ulises Daniel García Méndez <84304055+UliGarci@users.noreply.github.com>
2023-04-12 10:52:41 -0600

    Update TablesProduct.js

--- src/shared/Components/TablesProduct.js
+++ src/shared/Components/TablesProduct.js
@@ -8,7 +8,7 @@ export const TablesProduct = () => {
 
     useEffect(() => {
       const fetchData = async ()=>{
-        const result = await axios('http://34.201.151.124:4000/shop/products');
+        const result = await axios('http://34.201.251.124:5000/shop/products');
         setData(result.data);
         console.log('obtenido aq =>',result);
       };
